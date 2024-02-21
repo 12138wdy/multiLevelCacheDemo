43bdfff71b414e0ed1f9b38cad66d48987da81f0
wdy <myFarina@wdy.cn>
2024-02-21 12:26:46 +0800

    redis缓存

---
+++ src/main/java/com/heima/item/config/redisHandler.java
@@ -0,0 +1,46 @@
+package com.heima.item.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heima.item.pojo.Item;
+import com.heima.item.pojo.ItemStock;
+import com.heima.item.service.IItemService;
+import com.heima.item.service.IItemStockService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+public class redisHandler implements InitializingBean {
+
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    @Autowired
+    private IItemService itemService;
+    @Autowired
+    private IItemStockService stockService;
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        //查询数据缓存
+        //1.查询商品缓存
+        List<Item> items = itemService.list();
+        for (Item item : items) {
+            String json = MAPPER.writeValueAsString(item);
+            redisTemplate.opsForValue().set("item:id:" + item.getId(),json);
+        }
+
+        //2.查询库存缓存
+        List<ItemStock> itemStocks = stockService.list();
+        for (ItemStock itemStock : itemStocks) {
+            String json = MAPPER.writeValueAsString(itemStock);
+            redisTemplate.opsForValue().set("stock:id:" + itemStock.getId(), json);
+        }
+    }
+}
